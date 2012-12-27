d3a09e1cf4daa6f8a2eededa55ff4c812e11e858
Guillem Jover <guillem@hadrons.org>
2012-12-27 11:19:20 +0100

    Switch dehumanize_number() to use expand_number()
    The function is a duplicate of expand_number(), but covering less
    prefixes and with a slightly different function signature.
    
    Spotted-by: Peter da Silva <resuna@gmail.com>

--- src/dehumanize_number.c
+++ src/dehumanize_number.c
@@ -1,12 +1,5 @@
-/*	$NetBSD: dehumanize_number.c,v 1.2 2007/12/14 17:32:47 xtraeme Exp $	*/
-
 /*
- * Copyright (c) 2005, 2006 The NetBSD Foundation, Inc.
- * All rights reserved.
- *
- * This code is derived from software contributed to The NetBSD Foundation
- * by Julio M. Merino Vidal, developed as part of Google's Summer of Code
- * 2005 program.
+ * Copyright © 2012 Guillem Jover <guillem@hadrons.org>
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -16,99 +9,54 @@
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
+ * 3. The name of the author may not be used to endorse or promote products
+ *    derived from this software without specific prior written permission.
  *
- * THIS SOFTWARE IS PROVIDED BY THE NETBSD FOUNDATION, INC. AND CONTRIBUTORS
- * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
- * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
- * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE FOUNDATION OR CONTRIBUTORS
- * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
- * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
- * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
- * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
- * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
- * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
- * POSSIBILITY OF SUCH DAMAGE.
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES,
+ * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY
+ * AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL
+ * THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+ * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
+
 #include <sys/cdefs.h>
-#if defined(LIBC_SCCS) && !defined(lint)
-__RCSID("$NetBSD: dehumanize_number.c,v 1.2 2007/12/14 17:32:47 xtraeme Exp $");
-#endif /* LIBC_SCCS and not lint */
 
-#include <inttypes.h>
-#include <ctype.h>
-#include <stdlib.h>
-#include <string.h>
 #include <errno.h>
-#include <limits.h>
+#include <ctype.h>
+#include <libutil.h>
+#include <stdint.h>
+#include <stdlib.h>
 
-/*
- * Converts the number given in 'str', which may be given in a humanized
- * form (as described in humanize_number(3), but with some limitations),
- * to an int64_t without units.
- * In case of success, 0 is returned and *size holds the value.
- * Otherwise, -1 is returned and *size is untouched.
- *
- * TODO: Internationalization, SI units.
- */
 int
-dehumanize_number(const char *str, int64_t *size)
+dehumanize_number(const char *buf, int64_t *num)
 {
-	char *ep, unit;
-	const char *delimit;
-	long multiplier;
-	long long tmp, tmp2;
-	size_t len;
+	uint64_t rval;
+	int sign = 1;
+	int rc;
 
-	len = strlen(str);
-	if (len == 0) {
-		errno = EINVAL;
+	/* The current expand_number() implementation uses bit shifts, so
+	 * we cannot pass negative numbers, preserve the sign and apply it
+	 * later. */
+	while (isspace(*buf))
+		buf++;
+	if (*buf == '-') {
+		sign = -1;
+		buf++;
+	}
+
+	rc = expand_number(buf, &rval);
+	if (rc < 0)
+		return rc;
+	if (rval == UINT64_MAX && sign == -1) {
+		errno = ERANGE;
 		return -1;
 	}
-
-	multiplier = 1;
-
-	unit = str[len - 1];
-	if (isalpha((unsigned char)unit)) {
-		switch (tolower((unsigned char)unit)) {
-		case 'b':
-			multiplier = 1;
-			break;
-
-		case 'k':
-			multiplier = 1024;
-			break;
-
-		case 'm':
-			multiplier = 1024 * 1024;
-			break;
-
-		case 'g':
-			multiplier = 1024 * 1024 * 1024;
-			break;
-
-		default:
-			errno = EINVAL;
-			return -1; /* Invalid suffix. */
-		}
-
-		delimit = &str[len - 1];
-	} else
-		delimit = NULL;
-
-	errno = 0;
-	tmp = strtoll(str, &ep, 10);
-	if (str[0] == '\0' || (ep != delimit && *ep != '\0'))
-		return -1; /* Not a number. */
-	else if (errno == ERANGE && (tmp == LLONG_MAX || tmp == LLONG_MIN))
-		return -1; /* Out of range. */
-
-	tmp2 = tmp * multiplier;
-	tmp2 = tmp2 / multiplier;
-	if (tmp != tmp2) {
-		errno = ERANGE;
-		return -1; /* Out of range. */
-	}
-	*size = tmp * multiplier;
+	*num = rval * sign;
 
 	return 0;
 }
