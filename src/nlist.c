b1ec491f915c62c67c76cef2844421209b68618c
Guillem Jover <guillem@hadrons.org>
2019-06-15 14:33:32 +0200

    nlist: Check whether the nl argument is not NULL
    This prevents programming errors.
    
    Reported-by: Daniel Hodson <daniel@elttam.com.au>
    Based-on-patch-by: Daniel Hodson <daniel@elttam.com.au>
    Signed-off-by: Guillem Jover <guillem@hadrons.org>

--- src/nlist.c
+++ src/nlist.c
@@ -258,6 +258,10 @@ nlist(const char *name, struct nlist *list)
 {
 	int fd, n;
 
+	if (list == NULL) {
+		errno = EINVAL;
+		return (-1);
+	}
 	fd = open(name, O_RDONLY, 0);
 	if (fd < 0)
 		return (-1);
