guillemj/libbsd

commit 9fa06763a1afe0946a3a20e5bbdba72885cbade5

Port getprogname() to AIX
Get the program name from the COMM field from the proc filesystem.

We could use instead the information from the psinfo binary file under
/proc, but that seems to have a shorter string limit.

src/progname.c
@@ -62,6 +62,14 @@ getprogname(void)
 	/* getexecname(3) returns an absolute pathname, normalize it. */
 	if (__progname == NULL)
 		setprogname(getexecname());
+#elif defined(_AIX)
+	if (__progname == NULL) {
+		struct procentry64 procs;
+		pid_t pid = getpid ();
+
+		if (getprocs64(&procs, sizeof procs, NULL, 0, &pid, 1) > 0)
+			__progname = strdup(procs.pi_comm);
+	}
 #elif defined(_WIN32)
 	if (__progname == NULL) {
 		WCHAR *wpath = NULL;

commit 8f998d1d204c7a6218b49b021b74e6ac2e8fc642

progname: Include <procinfo.h> if available
We need this header on AIX. Missed transplanting the code from the AIX
porting system.

Fixes: commit 9fa06763a1afe0946a3a20e5bbdba72885cbade5

configure.ac
@@ -179,6 +179,7 @@ AC_CHECK_HEADERS([\
   pwd.h \
   grp.h \
   stdio_ext.h \
+  procinfo.h \
 ])
 
 # Checks for typedefs, structures, and compiler characteristics.

src/progname.c
@@ -35,6 +35,9 @@
 #include <errno.h>
 #include <string.h>
 #include <stdlib.h>
+#ifdef HAVE_PROCINFO_H
+#include <procinfo.h>
+#endif
 #ifdef _WIN32
 #include <Windows.h>
 #include <shlwapi.h>