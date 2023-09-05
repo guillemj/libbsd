guillemj/libbsd

commit 21d12b02112097f0c195dceb1892c95b7b957b36

build: On macOS do not build functions provided by the system
We have never built before on macOS, so we can exclude all the functions
that are currently provided in the system.

Closes: #1
Closes: !3

src/pwcache.c
@@ -103,6 +103,7 @@ st_hash(const char *name, size_t len, int tabsz)
 	return key % tabsz;
 }
 
+#ifndef HAVE_USER_FROM_UID
 /*
  * uidtb_start
  *	creates an an empty uidtb
@@ -124,7 +125,9 @@ uidtb_start(void)
 	}
 	return 0;
 }
+#endif
 
+#ifndef HAVE_GROUP_FROM_GID
 /*
  * gidtb_start
  *	creates an an empty gidtb
@@ -146,6 +149,7 @@ gidtb_start(void)
 	}
 	return 0;
 }
+#endif
 
 /*
  * usrtb_start
@@ -191,6 +195,7 @@ grptb_start(void)
 	return 0;
 }
 
+#ifndef HAVE_USER_FROM_UID
 /*
  * user_from_uid()
  *	caches the name (if any) for the uid. If noname clear, we always
@@ -251,7 +256,9 @@ user_from_uid(uid_t uid, int noname)
 	}
 	return ptr->name;
 }
+#endif
 
+#ifndef HAVE_GROUP_FROM_GID
 /*
  * group_from_gid()
  *	caches the name (if any) for the gid. If noname clear, we always
@@ -312,6 +319,7 @@ group_from_gid(gid_t gid, int noname)
 	}
 	return ptr->name;
 }
+#endif
 
 /*
  * uid_from_user()

configure.ac
@@ -148,9 +148,6 @@ AC_SEARCH_LIBS([SHA512Update], [md], [
 ])
 LIBS="$saved_LIBS"
 
-AM_CONDITIONAL([NEED_TRANSPARENT_LIBMD],
-  [test "x$need_transparent_libmd" = "xyes"])
-
 AS_CASE([$host_os],
   [*-gnu*], [
     # In old glibc versions (< 2.17) clock_gettime() is in librt.
@@ -305,8 +302,19 @@ AC_CHECK_FUNCS([\
   open_memstream \
   pstat_getproc \
   sysconf \
+  uid_from_user \
+  gid_from_group \
+  user_from_uid \
+  group_from_gid \
 ])
 
+need_arc4random=yes
+need_md5=yes
+need_nlist=yes
+need_strl=yes
+need_strmode=yes
+need_pwcache=yes
+need_fpurge=yes
 need_funopen=yes
 AS_CASE([$host_os],
   [*-musl*], [
@@ -319,12 +327,31 @@ AS_CASE([$host_os],
     need_funopen=yes
   ],
   [darwin*], [
+    # On macOS these are provided by the system, and libbsd has never built
+    # there, so we can avoid providing these with no ABI breakage.
+    need_arc4random=no
+    need_transparent_libmd=no
+    need_md5=no
+    need_nlist=no
+    need_strl=no
+    need_strmode=no
+    need_pwcache=no
+    need_fpurge=no
     # On macOS we do not have fopencookie(), and cannot implement it.
     need_funopen=no
   ],
 )
 
 AM_CONDITIONAL([HAVE_GETENTROPY], [test "x$ac_cv_func_getentropy" = "xyes"])
+
+AM_CONDITIONAL([NEED_ARC4RANDOM], [test "x$need_arc4random" = "xyes"])
+AM_CONDITIONAL([NEED_TRANSPARENT_LIBMD], [test "x$need_transparent_libmd" = "xyes"])
+AM_CONDITIONAL([NEED_MD5], [test "x$need_md5" = "xyes"])
+AM_CONDITIONAL([NEED_NLIST], [test "x$need_nlist" = "xyes"])
+AM_CONDITIONAL([NEED_STRL], [test "x$need_strl" = "xyes"])
+AM_CONDITIONAL([NEED_STRMODE], [test "x$need_strmode" = "xyes"])
+AM_CONDITIONAL([NEED_PWCACHE], [test "x$need_pwcache" = "xyes"])
+AM_CONDITIONAL([NEED_FPURGE], [test "x$need_fpurge" = "xyes"])
 AM_CONDITIONAL([NEED_FUNOPEN], [test "x$need_funopen" = "xyes"])
 AS_IF([test "x$need_funopen" = "xno" && \
        test "x$ac_cv_func_funopen" != "xyes" && \

include/Makefile.am
@@ -17,8 +17,6 @@ nobase_include_HEADERS = \
 	bsd/grp.h \
 	bsd/inttypes.h \
 	bsd/libutil.h \
-	bsd/md5.h \
-	bsd/nlist.h \
 	bsd/pwd.h \
 	bsd/readpassphrase.h \
 	bsd/stdio.h \
@@ -30,3 +28,15 @@ nobase_include_HEADERS = \
 	bsd/vis.h \
 	bsd/wchar.h \
 	# EOL
+
+if NEED_MD5
+nobase_include_HEADERS += \
+	bsd/md5.h \
+	# EOL
+endif
+
+if NEED_NLIST
+nobase_include_HEADERS += \
+	bsd/nlist.h \
+	# EOL
+endif

include/bsd/grp.h
@@ -44,8 +44,10 @@
 __BEGIN_DECLS
 int
 gid_from_group(const char *, gid_t *);
+#ifndef __APPLE__
 const char *
 group_from_gid(gid_t, int);
+#endif
 __END_DECLS
 
 #endif

include/bsd/pwd.h
@@ -44,8 +44,10 @@
 __BEGIN_DECLS
 int
 uid_from_user(const char *, uid_t *);
+#ifndef __APPLE__
 const char *
 user_from_uid(uid_t, int);
+#endif
 __END_DECLS
 
 #endif

include/bsd/stdlib.h
@@ -51,6 +51,7 @@
 #include <stdint.h>
 
 __BEGIN_DECLS
+#if !defined(__APPLE__)
 #if !defined(__GLIBC__) || \
     !__GLIBC_PREREQ(2, 36) || \
     !defined(_DEFAULT_SOURCE)
@@ -60,6 +61,7 @@ uint32_t arc4random_uniform(uint32_t upper_bound);
 #endif
 void arc4random_stir(void);
 void arc4random_addrandom(unsigned char *dat, int datlen);
+#endif
 
 int dehumanize_number(const char *str, int64_t *size);
 

include/bsd/string.h
@@ -41,10 +41,14 @@
 #include <sys/types.h>
 
 __BEGIN_DECLS
+#ifndef __APPLE__
 size_t strlcpy(char *dst, const char *src, size_t siz);
 size_t strlcat(char *dst, const char *src, size_t siz);
+#endif
 char *strnstr(const char *str, const char *find, size_t str_len);
+#ifndef __APPLE__
 void strmode(mode_t mode, char *str);
+#endif
 
 #if !defined(__GLIBC__) || \
     !__GLIBC_PREREQ(2, 25) || \

man/Makefile.am
@@ -177,13 +177,10 @@ dist_man_MANS = \
 	freezero.3bsd \
 	fmtcheck.3bsd \
 	fparseln.3bsd \
-	fpurge.3bsd \
 	getbsize.3bsd \
 	getmode.3bsd \
 	getpeereid.3bsd \
 	getprogname.3bsd \
-	gid_from_group.3bsd \
-	group_from_gid.3bsd \
 	heapsort.3bsd \
 	humanize_number.3bsd \
 	le16dec.3bsd \
@@ -193,15 +190,12 @@ dist_man_MANS = \
 	le64dec.3bsd \
 	le64enc.3bsd \
 	libbsd.7 \
-	md5.3bsd \
 	mergesort.3bsd \
-	nlist.3bsd \
 	pidfile.3bsd \
 	pidfile_close.3bsd \
 	pidfile_open.3bsd \
 	pidfile_remove.3bsd \
 	pidfile_write.3bsd \
-	pwcache.3bsd \
 	queue.3bsd \
 	radixsort.3bsd \
 	readpassphrase.3bsd \
@@ -219,9 +213,6 @@ dist_man_MANS = \
 	sl_init.3bsd \
 	sradixsort.3bsd \
 	stringlist.3bsd \
-	strlcat.3bsd \
-	strlcpy.3bsd \
-	strmode.3bsd \
 	strnstr.3bsd \
 	strnunvis.3bsd \
 	strnvis.3bsd \
@@ -244,13 +235,52 @@ dist_man_MANS = \
 	timespecsub.3bsd \
 	timeval.3bsd \
 	tree.3bsd \
-	uid_from_user.3bsd \
 	unvis.3bsd \
-	user_from_uid.3bsd \
 	vis.3bsd \
 	wcslcat.3bsd \
 	wcslcpy.3bsd \
 	# EOL
+
+if NEED_MD5
+dist_man_MANS += \
+	md5.3bsd \
+	# EOL
+endif
+
+if NEED_NLIST
+dist_man_MANS += \
+	nlist.3bsd \
+	# EOL
+endif
+
+if NEED_STRL
+dist_man_MANS += \
+	strlcat.3bsd \
+	strlcpy.3bsd \
+	# EOL
+endif
+
+if NEED_STRMODE
+dist_man_MANS += \
+	strmode.3bsd \
+	# EOL
+endif
+
+if NEED_PWCACHE
+dist_man_MANS += \
+	pwcache.3bsd \
+	uid_from_user.3bsd \
+	user_from_uid.3bsd \
+	gid_from_group.3bsd \
+	group_from_gid.3bsd \
+	# EOL
+endif
+
+if NEED_FPURGE
+dist_man_MANS += \
+	fpurge.3bsd \
+	# EOL
+endif
 
 if NEED_FUNOPEN
 dist_man_MANS += \

commit 6777eb62b8ee2b569454dc9b1d27e51a7dbf8764

pwcache: Do not declare uidtb and gidtb when not used
When the system provides implementations for user_from_uid() or
group_from_gid() we are not using these variables, so better not declare
them.

Fixes: commit 21d12b02112097f0c195dceb1892c95b7b957b36

src/pwcache.c
@@ -83,8 +83,12 @@ typedef struct gidc {
  * cache both hits and misses.
  */
 
+#ifndef HAVE_USER_FROM_UID
 static UIDC **uidtb;	/* uid to name cache */
+#endif
+#ifndef HAVE_GROUP_FROM_GID
 static GIDC **gidtb;	/* gid to name cache */
+#endif
 static UIDC **usrtb;	/* user name to uid cache */
 static GIDC **grptb;	/* group name to gid cache */
 